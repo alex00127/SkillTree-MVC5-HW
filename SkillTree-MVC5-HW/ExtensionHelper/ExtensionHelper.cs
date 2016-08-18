76287a033a5430c737672a751d9227f8b956f2f4
邱彥彰 <alex00127@hotmail.com>
2016-08-18 16:50:23 +0800

    Ceate Html helper for Category color

---
+++ SkillTree-MVC5-HW/ExtensionHelper/ExtensionHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SkillTree_MVC5_HW.Models.ViewModels;
+
+namespace SkillTree_MVC5_HW.ExtensionHelper
+{
+    public static class ExtensionHelper
+    {
+        public static MvcHtmlString SetColorByCategory(this HtmlHelper helper,CategoryEnum category )
+        {
+
+            return MvcHtmlString.Create(StringColor(category.ToString(), category == CategoryEnum.Expand ? "red" : "blue"));
+
+        }
+
+        private static string StringColor(string category, string color)
+        {
+            return $"<font color='{color}'>{category}</font>";
+        }
+    }
+}
